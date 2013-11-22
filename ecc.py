c300edc31299b3a91ea19b829ae20f164ba17826
Benjamin Mestdagh <benjaminmestdagh@gmail.com>
2013-11-22 15:50:54 +0100

    Added ECC class

--- ecc.py
+++ ecc.py
@@ -1,10 +1,3 @@
-import euclidextended
-
-class Point:
-    def __init__(self, x=0,y=0):
-        self.x = x
-        self.y = y
-
 class EccCalculator:
     def __init__(self, a, b, p):
         self.a = a
@@ -72,55 +65,9 @@ class EccCalculator:
 
         raise ValueError('The point you entered is not in the curve!') 
 
+class Point:
+    def __init__(self, x=0,y=0):
+        self.x = x
+        self.y = y
 
-def enterNumber(text):
-    number = input(text)
-    if(number.isdigit()):
-        return int(number)
-    else:
-        raise ValueError('Please enter a number!')
 
-def printMenu():
-    choice = None
-    while(choice == None or choice > 2 or choice < 0):
-        print('\n0: Exit\n1: Show all points in the curve\n2: Calculate multiplies')
-        choice = enterNumber('Make your choice: ')
-
-    return choice
-
-## Starting point for the application ##
-try:
-    print('ECC Calculator')
-    print('--------------')
-    print('Created in 2013 by Benjamin Mestdagh\n')
-   
-    a = enterNumber('Enter a: ')
-    b = enterNumber('Enter b: ')
-    p = enterNumber('Enter p: ')
-    calculator = EccCalculator(a,b,p)
-
-    choice = printMenu()
-
-    while(choice > 0):
-        if(choice == 1):
-            print('\nAll points in the curve (a={}, b={}, p={}):'.format(a,b,p))
-            results = calculator.calculateAllPoints()
-            for point in results:
-                print('({},{})'.format(point.x, point.y))
-        else:
-            n = enterNumber('Enter n: ')
-            x = enterNumber('Enter x: ')
-            y = enterNumber('Enter y: ')
-       
-            try:
-                print('\nThe multiplies:')
-                print('\n'.join(calculator.calculateG(n, x, y)))
-            except ValueError as e:
-                print(e)
-
-        choice = printMenu()
-    else:
-        print('Bye!')
-
-except ValueError as e:
-    print(e)
---
+++ run-ecc.py
@@ -0,0 +1,54 @@
+import euclidextended
+import ecc
+
+def enterNumber(text):
+    number = input(text)
+    if(number.isdigit()):
+        return int(number)
+    else:
+        raise ValueError('Please enter a number!')
+
+def printMenu():
+    choice = None
+    while(choice == None or choice > 2 or choice < 0):
+        print('\n0: Exit\n1: Show all points in the curve\n2: Calculate multiplies')
+        choice = enterNumber('Make your choice: ')
+
+    return choice
+
+## Starting point for the application ##
+try:
+    print('ECC Calculator')
+    print('--------------')
+    print('Created in 2013 by Benjamin Mestdagh\n')
+   
+    a = enterNumber('Enter a: ')
+    b = enterNumber('Enter b: ')
+    p = enterNumber('Enter p: ')
+    calculator = ecc.EccCalculator(a,b,p)
+
+    choice = printMenu()
+
+    while(choice > 0):
+        if(choice == 1):
+            print('\nAll points in the curve (a={}, b={}, p={}):'.format(a,b,p))
+            results = calculator.calculateAllPoints()
+            for point in results:
+                print('({},{})'.format(point.x, point.y))
+        else:
+            n = enterNumber('Enter n: ')
+            x = enterNumber('Enter x: ')
+            y = enterNumber('Enter y: ')
+       
+            try:
+                print('\nThe multiplies:')
+                print('\n'.join(calculator.calculateG(n, x, y)))
+            except ValueError as e:
+                print(e)
+
+        choice = printMenu()
+    else:
+        print('Bye!')
+
+except ValueError as e:
+    print(e)
